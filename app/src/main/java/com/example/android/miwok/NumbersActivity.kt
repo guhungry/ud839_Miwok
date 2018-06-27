1e6b050c92b5e8af6eec88ea1713fab7863f1236
Woraphot Chokratanasombat <wc@asiola.com>
2018-06-27 16:01:21 +0700

    [Numbers Activity] Change: Show all number using string

--- app/src/main/java/com/example/android/miwok/NumbersActivity.kt
+++ app/src/main/java/com/example/android/miwok/NumbersActivity.kt
@@ -2,11 +2,23 @@ package com.example.android.miwok
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import com.example.android.miwok.adapters.WordAdapter
+import kotlinx.android.synthetic.main.activity_numbers.*
 
 class NumbersActivity : AppCompatActivity() {
+    val list = arrayListOf("one", "two", "three", "four", "five", "six", "seven", "eight", "nine", "ten")
+    var adapter: WordAdapter? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_numbers)
+
+        setupListWord()
+    }
+
+    private fun setupListWord() {
+        adapter = WordAdapter(this, list)
+
+        list_word.adapter = adapter
     }
 }
---
+++ app/src/main/java/com/example/android/miwok/adapters/WordAdapter.kt
@@ -0,0 +1,21 @@
+package com.example.android.miwok.adapters
+
+import android.content.Context
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.ArrayAdapter
+import com.example.android.miwok.R
+import kotlinx.android.synthetic.main.word_list_item.view.*
+
+class WordAdapter(context: Context, list: ArrayList<String>? = null): ArrayAdapter<String>(context, R.layout.word_list_item, list) {
+    val inflater = LayoutInflater.from(context)
+
+    override fun getView(position: Int, convertView: View?, parent: ViewGroup?): View {
+        val view = if (convertView != null) convertView else inflater.inflate(R.layout.word_list_item, parent, false)
+        val item = getItem(position)
+
+        view.english.text = item
+        return view
+    }
+}
